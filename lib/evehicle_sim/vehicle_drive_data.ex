4db9e3189d752723fab7270c671ffd5ee313b957
robert4361 <robert4361@gmail.com>
2024-10-16 11:30:33 +0200

    Add function to turn row from csv file to struct

---
+++ lib/evehicle_sim/vehicle_drive_data.ex
@@ -0,0 +1,85 @@
+defmodule EvehicleSim.VehicleDriveData do
+  alias __MODULE__
+
+  @type t :: %VehicleDriveData{
+          milliseconds_since_epoch: integer(),
+          speed: float(),
+          watt: float(),
+          ampere: float(),
+          altitude: float(),
+          gps_speed: float(),
+          vehicle_body_temperature: integer(),
+          battery_percentage: integer(),
+          battery_voltage: float(),
+          battery_capacity: integer(),
+          battery_temperature: integer(),
+          remaining_mileage: float(),
+          total_mileage: float(),
+          latitude: float(),
+          longitude: float()
+        }
+
+  defstruct [
+    :milliseconds_since_epoch,
+    :speed,
+    :watt,
+    :ampere,
+    :altitude,
+    :gps_speed,
+    :vehicle_body_temperature,
+    :battery_percentage,
+    :battery_voltage,
+    :battery_capacity,
+    :battery_temperature,
+    :remaining_mileage,
+    :total_mileage,
+    :latitude,
+    :longitude
+  ]
+
+  @fields [
+    :milliseconds_since_epoch,
+    :speed,
+    :watt,
+    :ampere,
+    :altitude,
+    :gps_speed,
+    :vehicle_body_temperature,
+    :battery_percentage,
+    :battery_voltage,
+    :battery_capacity,
+    :battery_temperature,
+    :remaining_mileage,
+    :total_mileage,
+    :latitude,
+    :longitude
+  ]
+
+  @spec new(String.t()) :: t()
+  def new(row_data) do
+    fields =
+      row_data
+      |> Enum.zip(@fields)
+      |> Enum.into(%{}, fn {value, key} -> {key, cast_value(key, value)} end)
+
+    struct(__MODULE__, fields)
+  end
+
+  defp cast_value(:milliseconds_since_epoch, value), do: String.to_integer(value)
+  defp cast_value(:speed, value), do: String.to_float(value)
+  defp cast_value(:watt, value), do: String.to_float(value)
+  defp cast_value(:ampere, value), do: String.to_float(value)
+  defp cast_value(:altitude, value), do: String.to_float(value)
+  defp cast_value(:gps_speed, value), do: String.to_float(value)
+  defp cast_value(:vehicle_body_temperature, value), do: String.to_integer(value)
+  defp cast_value(:battery_percentage, value), do: String.to_integer(value)
+  defp cast_value(:battery_voltage, value), do: String.to_float(value)
+  defp cast_value(:battery_capacity, value), do: String.to_integer(value)
+  defp cast_value(:battery_temperature, value), do: String.to_integer(value)
+  defp cast_value(:remaining_mileage, value), do: String.to_float(value)
+  defp cast_value(:total_mileage, value), do: String.to_float(value)
+  defp cast_value(:latitude, value), do: String.to_float(value)
+  defp cast_value(:longitude, value), do: String.to_float(value)
+
+  defp cast_value(_, value), do: value
+end
