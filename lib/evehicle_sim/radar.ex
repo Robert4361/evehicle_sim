064b55df5206cde29519baf401770972b4859e04
robert4361 <robert4361@gmail.com>
2024-10-16 12:19:00 +0200

    Add a basic struct structure and create function

---
+++ lib/evehicle_sim/radar.ex
@@ -0,0 +1,31 @@
+defmodule EvehicleSim.Radar do
+  alias __MODULE__
+
+  @type t :: %Radar{
+          id: integer(),
+          max_speed: integer(),
+          max_duration: integer(),
+          max_distance: integer(),
+          radar_address: String.t(),
+          gps_width: float(),
+          gps_length: float()
+        }
+  defstruct [
+    :id,
+    :max_speed,
+    :max_duration,
+    :max_distance,
+    :radar_address,
+    :gps_width,
+    :gps_length
+  ]
+
+  @spec new([{String.t(), String.t()}]) :: t()
+  def new(radar_info) do
+    fields =
+      radar_info
+      |> Map.new(fn {key, value} -> {String.to_atom(key), value} end)
+
+    struct(__MODULE__, fields)
+  end
+end
