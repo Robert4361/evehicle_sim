a1865d0cbd9c679e5dbac6377c98138ae05e59e0
robert4361 <robert4361@gmail.com>
2024-10-18 11:21:34 +0200

    Add traffic ticket module with create function

---
+++ lib/evehicle_sim/traffic_ticket.ex
@@ -0,0 +1,48 @@
+defmodule EvehicleSim.TrafficTicket do
+  alias EvehicleSim.TrafficTicket
+
+  @type t() :: %TrafficTicket{
+          vehicle_id: integer(),
+          time_start: integer(),
+          time_end: integer(),
+          speed: float(),
+          gps_width: float(),
+          gps_length: float(),
+          radar_gps_width: float(),
+          radar_gps_length: float()
+        }
+
+  defstruct [
+    :vehicle_id,
+    :time_start,
+    :time_end,
+    :speed,
+    :gps_width,
+    :gps_length,
+    :radar_gps_width,
+    :radar_gps_length
+  ]
+
+  @spec new(integer(), integer(), integer, float(), float(), float(), float(), float()) :: t()
+  def new(
+        vehicle_id,
+        time_start,
+        time_end,
+        speed,
+        gps_width,
+        gps_length,
+        radar_gps_width,
+        radar_gps_length
+      ) do
+    %TrafficTicket{
+      vehicle_id: vehicle_id,
+      time_start: time_start,
+      time_end: time_end,
+      speed: speed,
+      gps_width: gps_width,
+      gps_length: gps_length,
+      radar_gps_width: radar_gps_width,
+      radar_gps_length: radar_gps_length
+    }
+  end
+end
