2c38a8c04e2d60ee97dfaf9dddc250233319f5b6
robert4361 <robert4361@gmail.com>
2024-10-20 21:27:39 +0200

    Add module for calculating distance between 2 gps locations

---
+++ lib/evehicle_sim/gps_distance_calculator.ex
@@ -0,0 +1,22 @@
+defmodule EvehicleSim.GpsDistanceCalculator do
+  def calculate_distance_km(lat1, lon1, lat2, lon2) do
+    x1 = lat1 * (:math.pi() / 180)
+    y1 = lon1 * (:math.pi() / 180)
+    x2 = lat2 * (:math.pi() / 180)
+    y2 = lon2 * (:math.pi() / 180)
+
+    a =
+      :math.pow(:math.sin((x2 - x1) / 2), 2) + :math.cos(x1) +
+        :math.cos(x2) * :math.pow(:math.sin((y2 - y1) / 2), 2)
+
+    angle = 2 * :math.asin(min(1, :math.sqrt(a)))
+
+    angle_degrees = angle * 180 / :math.pi()
+
+    60 * angle_degrees * 1.852
+  end
+
+  def calculate_distance_m(lat1, lon1, lat2, lon2) do
+    calculate_distance_km(lat1, lon1, lat2, lon2) * 1000
+  end
+end
