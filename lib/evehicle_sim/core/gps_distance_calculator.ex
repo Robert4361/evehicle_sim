138c2b75c87aa3a5ebf8f8fc6a4c6fa787f3b17e
robert4361 <robert4361@gmail.com>
2024-10-28 22:53:16 +0100

    Fix Haversine formula for distance calculation

--- lib/evehicle_sim/core/gps_distance_calculator.ex
+++ lib/evehicle_sim/core/gps_distance_calculator.ex
@@ -1,22 +1,27 @@
 defmodule EvehicleSim.Core.GpsDistanceCalculator do
+  @earth_radius 6371.009
+
+  # formula taken from https://www.vcalc.com/wiki/vcalc/haversine-distance
   def calculate_distance_km(lat1, lon1, lat2, lon2) do
-    x1 = lat1 * (:math.pi() / 180)
-    y1 = lon1 * (:math.pi() / 180)
-    x2 = lat2 * (:math.pi() / 180)
-    y2 = lon2 * (:math.pi() / 180)
+    lat1 = degrees_to_radians(lat1)
+    lon1 = degrees_to_radians(lon1)
+    lat2 = degrees_to_radians(lat2)
+    lon2 = degrees_to_radians(lon2)
 
-    a =
-      :math.pow(:math.sin((x2 - x1) / 2), 2) + :math.cos(x1) +
-        :math.cos(x2) * :math.pow(:math.sin((y2 - y1) / 2), 2)
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
 
-    angle = 2 * :math.asin(min(1, :math.sqrt(a)))
+    a = :math.sin(dlat / 2) |> :math.pow(2)
+    b = :math.sin(dlon / 2) |> :math.pow(2)
+    c = :math.cos(lat1) * :math.cos(lat2)
+    d = :math.sqrt(a + b * c)
 
-    angle_degrees = angle * 180 / :math.pi()
-
-    60 * angle_degrees * 1.852
+    2 * :math.asin(d) * @earth_radius
   end
 
   def calculate_distance_m(lat1, lon1, lat2, lon2) do
     calculate_distance_km(lat1, lon1, lat2, lon2) * 1000
   end
+
+  def degrees_to_radians(value), do: value * (:math.pi() / 180)
 end
