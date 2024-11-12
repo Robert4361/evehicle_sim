5dd50c5a56913fa1f7c7cc5f2343e3007a5e62cc
robert4361 <robert4361@gmail.com>
2024-11-12 15:45:02 +0100

    Start implementing radar server functions

--- lib/evehicle_sim/runtime/workers/radar_server.ex
+++ lib/evehicle_sim/runtime/workers/radar_server.ex
@@ -4,20 +4,33 @@ defmodule EvehicleSim.Runtime.Workers.RadarServer do
   alias EvehicleSim.Core.FileReaders.TxtFileReader
   alias EvehicleSim.Core.Radar
 
+  # client
   def start_link(file_name) do
     case TxtFileReader.open_file(file_name) do
       {:ok, data} ->
         radar = Radar.new(data)
         name = {:via, Registry, {EvehicleSim.RadarRegistry, "radar_#{radar.id}", radar}}
-        GenServer.start_link(__MODULE__, nil, name: name)
+
+        GenServer.start_link(
+          __MODULE__,
+          %{max_speed: radar.max_speed, max_duration: radar.max_duration},
+          name: name
+        )
 
       {:error, reason} ->
         {:error, reason}
     end
   end
 
+  @spec send_row(String.t(), {integer(), EvehicleSim.Core.VehicleDriveData.t()}) :: term()
+  def send_row(name, row) do
+    via_tuple = {:via, Registry, {EvehicleSim.RadarRegistry, name}}
+    GenServer.cast(via_tuple, {:vehicle, row})
+  end
+
+  # server
   @impl true
-  def init(_init_arg) do
-    {:ok, nil}
+  def init(radar_data) do
+    {:ok, radar_data |> Map.put(:start_data, nil) |> Map.put(:end_data, nil)}
   end
 end
